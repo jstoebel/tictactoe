94d8c0a9df74f5ac5bc9af94c5b93e5f3d9bd5d1
Jacob Stoebel <stoebelj@berea.edu>
2016-04-12 09:07:44 -0400

    working on recursion

--- design.md
+++
@@ -1,5 +0,0 @@
-
-- for each game state
-    - get all possible moves
-    - get the score of that move (no win=0, computer win=10-depth, computer lose=-10+depth)
-    - pick the move with the best score (largest for computer, smallest for player.)    
--- script2.js
+++ script2.js
@@ -1,22 +1,24 @@
-var Game = function(board, compTurn){
+var Game = function(board, player, depth){
     //board: a 2d array of representing the game state
-    //turn: whose turn is it? ('x' or 'o')
+    //player: whose turn is it? ('x' or 'o')
     //1 = computer, 0 = human
+    //depth: how many turns have occured? Start counting with 0
 
     this.board = board;
-    this.compTurn = compTurn;
+    this.player = player;
+    this.depth = depth;
 
 
     this.win = function(){
         //determines if the game is in a winning state
         //we check win state before a turn is taken, therfore if there is a winner,
         //it is always the opposing player.
-        var player = Number(!this.compTurn);
+
+        var playerToCheck = Number(!this.player)
 
         _checkSequences = function(arrs, player){
             //checks if entire 3 item array is filled with player's value
 
-            console.log(arrs);
             for (i in arrs){
                 var arr = arrs[i];
                 var allMatch = arr.every(function(i){
@@ -80,33 +82,110 @@ var Game = function(board, compTurn){
             return _checkSequences([seq], player)
         }
 
-        console.log(_checkSecondDiagonal(player, board))
-        // return (this._checkColumn(player) || this._checkRow(player) || 
-        //     this._checkPrimeDiagonal(player) || this._checkSecondDiagonal(player))
+        return (_checkColumns(playerToCheck, board) || _checkRows(playerToCheck, board) || 
+            _checkPrimeDiagonal(playerToCheck, board) || _checkSecondDiagonal(playerToCheck, board))
     }
 
     this.score = function() {
         //returns the score of the current game state
+
+        if (this.win()){
+            if(this.player){
+                return 10 - depth;
+            } else {
+                return -10;
+            }
+        } else {
+            return depth - 0;
+        }
+    }
+
+    this.step = function(move){
+        //creates a new game object based on move and returns the result
+        //move: a 2d array of coordinates to move
+        //returns a new game with move applied
+            //player is flipped
+            //depth is incremented by 1
+
+        var newBoard = this.board;
+        newBoard[move[0]][move[1]] = this.player;
+
+        var turn = Number(!this.player);
+
+        var depth = this.depth + 1;
+        return new Game(newBoard, turn, depth )
     }
 
     this.getPossibleMoves = function() {
         //returns 2d array of all possible moves. examlpe [[0,1], [0, 2]...]
+
+        var possibleMoves = [];
+        for (r in this.board){
+            for (c in this.board){
+                if (this.board[r][c] === null){
+                    possibleMoves.push([r, c]);
+                }
+            }
+        }
+        return possibleMoves;
     }
 
     this.minimax = function() {
+        //RECURSION HAPPENS HERE
         //recursivly determines all possible outcomes starting from current state
-        //returns the most desired move
+        //returns the object structured as {bestScore: move}
+
+        if(this.score() != 0){
+            return this.score();
+        } else {
+            //recursion!
+            var outcomes = [];
+            var possibleMoves = this.getPossibleMoves();
+            for (var m=0; m<possibleMoves.length; m++){
+                var move = possibleMoves[m];
+                newGame = this.step(move);
+                score = newGame.minimax();
+
+                outcomes.push({score: move})
+            }
+            
+            //look at all of the outcomes, 
+            //decide which is the best and return
+            
+            var bestScore = -(11 * this.player); //lower than lowest possible
+            var bestOutcome = null;
+
+            var betterScore = function(score) {
+                //returns if the given score is better than bestScore
+                //based on whose turn it is
+
+                if (this.player == 1 ){
+                    return score > bestScore
+                } else {
+                    return score < bestScore
+                }
+            }
+
+            for (o in outcomes){
+                var outcome = outcomes[o];
+                var outcomeScore = outcome.keys()[0] 
+                if ( betterScore( outcomeScore )){
+                    bestScore  = outcomeScore;
+                    bestOutcome = outcome
+                }
+            }
+
+            return bestOutcome;
+        }
     }
 
 };
 
 var board = [
-    [1, 1, 1],
-    [null,1,null],
-    [1, null, null]
+    [0,     null,   1],
+    [1,     null,   null],
+    [1,     0,      0]
 ]
 
-game = new Game(board, false)
-console.log(game.win())
-
-
+game = new Game(board, 1, 0)
+game.minimax()
